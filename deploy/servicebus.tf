bd823d1fa8ccec5df9673df7a3972d93af04caf7
Jorge Turrado <jorge_turrado@hotmail.es>
2024-11-19 01:56:39 +0100

    .
    Signed-off-by: Jorge Turrado <jorge_turrado@hotmail.es>

--- deploy/servicebus.tf
+++ deploy/servicebus.tf
@@ -20,8 +20,8 @@ resource "azurerm_servicebus_namespace_authorization_rule" "autoscaler" {
   name         = "autoscaler"
   namespace_id = azurerm_servicebus_namespace.servicebus.id
 
-  listen = false
-  send   = false
+  listen = true
+  send   = true
   manage = true
 }
 
